fd7d0a3a65dcca8f6d55607c9d31086b7ff16452
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-30 10:09:31 -0400

    feat: remove Batch model and its related fields from models.py

--- src/applications/main/models.py
+++ src/applications/main/models.py
@@ -6,32 +6,3 @@ from .modules.employee.models.employee_model import employee_model as employee_m
 from .modules.inventory.models.provider_model import Provider as Provider
 from .modules.inventory.models.inventory_recipt_model import StockEntry as StockEntry
 from .modules.inventory.models.stock_movement import StockMovement_model as StockMovement_model
-
-
-from django.db import models
-from django.utils import timezone
-
-class Batch(models.Model):
-    class Meta:
-        db_table = "batch"
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    provider = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name="provided_batches",
-        limit_choices_to={"role": UserRoles.CUSTOMER} # TODO: make this provider
-    )
-
-    accepted_by = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name="accepted_batches",
-        limit_choices_to={"role": UserRoles.CUSTOMER} # TODO: make this employee
-    )
-    unitperbatch = models.IntegerField()
-    acquisition_price = models.FloatField()
-    datetime = models.DateTimeField(null=True, blank=True)
