59c49f1fa1fd4745f286158d073eeb4787333e11
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-02 10:01:04 -0400

    feat: add categories view and update URL routing

---
+++ src/applications/main/modules/store/views/categories/categories_view.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from ....product.services.product.product_service import ProductService
+
+def categories_view(request):
+    product = ProductService()
+    context = {
+        "storename": "Petal",
+        "categories": product.getAllCategories()
+    }
+    return render(request, "store/categories/categories_page.html",context)
