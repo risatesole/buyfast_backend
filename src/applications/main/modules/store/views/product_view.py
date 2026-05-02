eebce2ddb06d04fe26a2b18aff6240dc72a91a86
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-02 10:17:02 -0400

    feat: implement sell_product function to handle product sales and stock updates

--- src/applications/main/modules/store/views/product_view.py
+++ src/applications/main/modules/store/views/product_view.py
@@ -14,44 +14,14 @@ from django.db import transaction
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
-from ...inventory.models.stock_movement import StockMovement_model
 from ...product.models.model_product import Product
-
+from ..services.sell_product import sell_product
 
 @login_required
 def store_front_buy_product(request, product_id):
     product = get_object_or_404(Product, id=product_id)
 
     if request.method == "POST":
+        sell_product(product, request.user)
 
-        with transaction.atomic():
-
-            # get current balance from last movement
-            last_movement = (
-                StockMovement_model.objects
-                .filter(product=product)
-                .order_by("-id")
-                .first()
-            )
-
-            previous_balance = last_movement.balance if last_movement else 0
-
-            # we sell 1 unit
-            quantity_sold = 1
-            new_balance = previous_balance - quantity_sold
-
-            # register SALE movement
-            StockMovement_model.objects.create(
-                product=product,
-                movement_type="customer_sell",
-                quantity=quantity_sold,
-                balance=new_balance,
-                document_reference=f"BUY-{product.id}-{request.user.id}", # type: ignore
-            )
-
-            # deactivate product if no stock left
-            if new_balance <= 0:
-                product.status = "DEACTIVATED"
-                product.save()
-
-    return redirect("product_page", product_id=product.id) # type: ignore
+    return redirect("product_page", product_id=product.id)  # type: ignore
