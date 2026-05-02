46eff2fc8709363827236a3f79d3720771375682
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-02 10:38:07 -0400

    refactor: enhance docstring in storefront_view for improved clarity

--- src/applications/main/modules/store/views/storefront/storefront.py
+++ src/applications/main/modules/store/views/storefront/storefront.py
@@ -2,5 +2,9 @@ from ..context_handlers.storefront_context_handler import store_front_context_ha
 from django.shortcuts import render
 
 def storefront_view(request):
+    """
+    Renders the storefront homepage using context provided by the 
+    storefront context handler.
+    """
     context = store_front_context_handler()
     return render(request, "pages/home/index.html", context)
