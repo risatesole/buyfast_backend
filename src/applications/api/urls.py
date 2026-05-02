6ed3c7b4f66205a08edb497730bf517c0906c502
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-02 17:48:13 -0400

    feat: add productcategories view and update urls for product categories endpoint

--- src/applications/api/urls.py
+++ src/applications/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import health, products
+from .views import health, products, productcategories
 
 urlpatterns = [
     path('health/', health),
-    path('products/', products)
+    path('products/', products),
+    path("productcategories/",productcategories)
 ]
--- src/applications/api/views.py
+++ src/applications/api/views.py
@@ -1,13 +1,13 @@
-from rest_framework.decorators import api_view
+from ..main.modules.product.services.product.product_service import ProductService
+from ..main.modules.product.models.model_product import Product
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def health(request):
     return Response({
         "status": "ok"
     })
-
-from ..main.modules.product.services.product.product_service import ProductService
 
 @api_view(['GET'])
 def products(request):
@@ -17,4 +17,18 @@ def products(request):
     return Response({
         "status": "ok",
         "data": products
+    })
+
+@api_view(['GET'])
+def productcategories(request):
+    categories = [
+        {
+            "value": key,
+            "label": label
+        }
+        for key, label in Product.CATEGORY_CHOICE
+    ]
+    return Response({
+        "status": "ok",
+        "data": categories
     })
