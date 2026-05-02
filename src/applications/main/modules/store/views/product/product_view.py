5a4b1f8895436a1dc9cb1944c745ab2175b30f88
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-02 10:29:51 -0400

    refactor: update docstring formatting in storefront_product_page for improved readability

--- src/applications/main/modules/store/views/product/product_view.py
+++ src/applications/main/modules/store/views/product/product_view.py
@@ -3,7 +3,10 @@ from ....product.models.model_product import Product
 from django.shortcuts import get_object_or_404
 
 def storefront_product_page(request, product_id):
-    """View that renders a product detail page by fetching a Product by ID or returning 404 if not found."""
+    """
+    View that renders a product detail page by fetching a Product by 
+    ID or returning 404 if not found.
+    """
     product = get_object_or_404(Product, id=product_id)
     return render(request, "store/product/product_page.html", {
         "product": product
