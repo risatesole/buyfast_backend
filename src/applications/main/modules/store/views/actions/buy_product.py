7d32b8ca0cec7389e7bcef592c6cc4a6d2c13611
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-02 10:26:45 -0400

    refactor: update docstring in store_front_buy_product and reorganize import in urls.py

--- src/applications/main/modules/store/views/actions/buy_product.py
+++ src/applications/main/modules/store/views/actions/buy_product.py
@@ -5,9 +5,11 @@ from ....product.models.model_product import Product
 
 @login_required
 def store_front_buy_product(request, product_id):
+    """
+    View that handles authenticated product purchase by processing 
+    POST requests and redirecting to the product page.
+    """
     product = get_object_or_404(Product, id=product_id)
-
     if request.method == "POST":
         sell_product(product, request.user)
-
     return redirect("product_page", product_id=product.id)  # type: ignore
