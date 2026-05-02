881335555a3b44f83d4079c07f3a59a5f1936de4
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-02 11:45:04 -0400

    feat: add success and error message display in product purchase flow

--- src/applications/main/modules/store/views/actions/buy_product.py
+++ src/applications/main/modules/store/views/actions/buy_product.py
@@ -1,15 +1,19 @@
 from django.contrib.auth.decorators import login_required
-from ...services.sell_product import sell_product
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect
+
+from ...services.sell_product import sell_product
 from ....product.models.model_product import Product
 
 @login_required
 def store_front_buy_product(request, product_id):
-    """
-    View that handles authenticated product purchase by processing 
-    POST requests and redirecting to the product page.
-    """
     product = get_object_or_404(Product, id=product_id)
+
     if request.method == "POST":
-        sell_product(product, request.user)
-    return redirect("product_page", product_id=product.id)  # type: ignore
+        try:
+            sell_product(product, request.user)
+            messages.success(request, "Product purchased successfully.")
+        except Exception as e: 
+            messages.error(request, str(e))
+
+    return redirect("product_page", product_id=product.id) # type: ignore
