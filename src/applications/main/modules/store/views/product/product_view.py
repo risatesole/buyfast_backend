88a7b7889697b46fb1edc837e4cf15bba74de1dd
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-03 00:17:57 -0400

    feat: update product page title and logo to use storename variable

--- src/applications/main/modules/store/views/product/product_view.py
+++ src/applications/main/modules/store/views/product/product_view.py
@@ -9,5 +9,6 @@ def storefront_product_page(request, product_id):
     """
     product = get_object_or_404(Product, id=product_id)
     return render(request, "store/product/product_page.html", {
-        "product": product
+        "storename": "Petal",
+        "product": product,
     })
