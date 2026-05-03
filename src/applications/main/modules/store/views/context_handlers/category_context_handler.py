0f9622bff20ef56982b8f49bfcd7c990bb8bfe27
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-03 08:48:28 -0400

    feat: enhance category page by adding product status check and improving no products message

--- src/applications/main/modules/store/views/context_handlers/category_context_handler.py
+++ src/applications/main/modules/store/views/context_handlers/category_context_handler.py
@@ -2,11 +2,8 @@ from django.http import Http404
 from ....product.models.model_product import Product
 
 def store_category_context_handler(name):
-    """
-    Builds context for a category page by validating the category and 
-    retrieving active products.
-    """
     category_key = name.upper()
+
     valid_categories = [c[0] for c in Product.CATEGORY_CHOICE]
 
     if category_key not in valid_categories:
