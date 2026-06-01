e623006ac16384905c5ee2bfe513a5ab29ea190c
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-06-01 08:35:00 -0400

    feat(api): add me view to show users information

---
+++ src/applications/api/modules/auth/me_api_view.py
@@ -0,0 +1,32 @@
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from ...utils import CsrfExemptSessionAuthentication
+
+
+@api_view(['GET'])
+@authentication_classes([CsrfExemptSessionAuthentication])
+@permission_classes([IsAuthenticated])
+def me_api_view(request):
+    try:
+        user = request.user
+
+        return Response({
+            "status": "ok",
+            "data": {
+                "user": {
+                    "id": user.id,
+                    "firstname": user.first_name,
+                    "lastname": user.last_name,
+                    "email": user.email,
+                    "role": getattr(user, "role", None),
+                }
+            }
+        })
+
+    except Exception as e:
+        return Response({
+            "status": "error",
+            "message": str(e)
+        }, status=400)
+        
--- src/applications/api/urls.py
+++ src/applications/api/urls.py
@@ -4,7 +4,9 @@ from .modules.auth.signup_api_view import signup_api_view
 from .modules.auth.signin_api_view import signin_api_view
 from .modules.auth.signout_api_view import signout_api_view
 from .modules.auth.change_password import change_password_api_view
+from .modules.auth.me_api_view import me_api_view
 from .modules.store.buy_product_api_view import buy_product
+
 urlpatterns = [
     path('health/', health),
     path('products/', products),
@@ -14,4 +16,5 @@ urlpatterns = [
     path("signout/", signout_api_view ),
     path("change_password/",change_password_api_view),
     path("buy-product/", buy_product),
+    path("me/", me_api_view),
 ]
