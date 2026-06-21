b85f7788d3e64d671091c7d1159697400aa7151f
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-06-20 22:25:14 -0400

    feat(settings): enhance security with updated cookie settings for CORS and CSRF

--- config/settings.py
+++ config/settings.py
@@ -141,11 +141,16 @@ CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:3000"
     ","
 )
 
+SESSION_COOKIE_SAMESITE = "None"
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = "None"
+CSRF_COOKIE_SECURE = True
+
 # Required so the browser sends the session cookie cross-origin
 CORS_ALLOW_CREDENTIALS = True
 
 # Allow the CSRF cookie to be read by JS (needed for PATCH/POST from the browser)
-CSRF_COOKIE_SAMESITE = "Lax"
+# CSRF_COOKIE_SAMESITE = "Lax"
 CSRF_COOKIE_HTTPONLY = False  # JS needs to read it to send as header
 
 # ─────────────────────────────────────────────────────────────────────────────
