379e28c56783c138831c42403c3f42808a2d399e
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-06-23 14:19:41 -0400

    feat(products): implement custom paginated response for product tags

--- products/views/tags/product_tags_pagination.py
+++ products/views/tags/product_tags_pagination.py
@@ -1,8 +1,15 @@
 from rest_framework.pagination import CursorPagination
+from rest_framework.response import Response
 
 class ProductCursorPagination(CursorPagination):
     page_size = 20
     ordering = "-id"
-
     page_size_query_param = "page_size"
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            "data": data,
+            "next": self.get_next_link(),
+            "previous": self.get_previous_link(),
+        })
