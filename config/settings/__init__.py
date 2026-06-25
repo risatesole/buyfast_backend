5e7417e5048b6402d504918f99b882d312dc5825
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-06-25 17:34:00 -0400

    refactor(settings): Move middleware.py to middleware subdirectory

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,3 +1,3 @@
 from .base import *
 from .installed_apps.installed_apps import INSTALLED_APPS as INSTALLED_APPS
-from .middleware import MIDDLEWARE as MIDDLEWARE
+from .middleware.middleware import MIDDLEWARE as MIDDLEWARE
