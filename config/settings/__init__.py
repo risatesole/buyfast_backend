d81547314ce14e327d56fc669f6520f2871da8e1
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-06-25 18:04:09 -0400

    fix(settings): replace the import all to import each variable in config of static files

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,4 +1,6 @@
 from .base import *
 from .installed_apps.installed_apps import INSTALLED_APPS as INSTALLED_APPS
 from .middleware.middleware import MIDDLEWARE as MIDDLEWARE
-from .staticfiles.staticfiles import *
+from .staticfiles.staticfiles import STATIC_ROOT as STATIC_ROOT
+from .staticfiles.staticfiles import STATIC_URL as STATIC_URL
+from .staticfiles.staticfiles import STATICFILES_DIRS as STATICFILES_DIRS
