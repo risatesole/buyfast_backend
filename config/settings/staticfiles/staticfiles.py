672dc070ccf0ed77386d0f75de5210a3d1c86c93
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-06-25 22:17:54 -0400

    feat: make STATIC_URL configurable via environment variable

--- config/settings/staticfiles/staticfiles.py
+++ config/settings/staticfiles/staticfiles.py
@@ -1,5 +1,7 @@
+import os
+
 from config.settings.base_dir import BASE_DIR
 
-STATIC_URL = "/static/"
+STATIC_URL = os.getenv("STATIC_URL", "/static/")
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = []
