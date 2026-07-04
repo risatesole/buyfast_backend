b5c3717930ab15b4f1fa35c29edf21665136e187
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-07-04 14:58:40 -0400

    feat: add mock warning comment to orders detail endpoint
    - Add warning comment to indicate mock data implementation

--- orders/views/customers/orders_api_view.py
+++ orders/views/customers/orders_api_view.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
+# WAENING : mock
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([AllowAny])
