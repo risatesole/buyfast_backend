b39878733962de7f7f15ebba81162183a71eb498
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-07-08 18:33:03 -0400

    refactor: rename get_category_default to get_category_default_model_object
    Rename function to better reflect that it returns a Category model instance
    rather than a default value or ID.

--- products/default/usecases/categories/get_category_default.py
+++ products/default/usecases/categories/get_category_default.py
@@ -1,7 +1,7 @@
 from ...models import  Category
 from django.utils.text import slugify
 
-def get_category_default():
+def get_category_default_model_object():
     category, created = Category.objects.get_or_create(
         name="Electronics",
         defaults={
