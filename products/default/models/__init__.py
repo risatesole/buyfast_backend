a7e6202d4a01a6deda6ef6931639a86cb6c6ce5c
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-07-08 11:20:48 -0400

    refactor(products): move products to models folder in stead of models.py inside the products module

---
+++ products/default/models/__init__.py
@@ -0,0 +1,4 @@
+from .products_model import Category as Category
+from .products_model import ProductType as ProductType
+from .products_model import Product as Product
+from .products_model import ProductImage as ProductImage
