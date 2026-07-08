a81996f349fbe4c7864815b4451f550dc1769bbc
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-07-08 14:11:58 -0400

    refactor(products): remove ProductImage model and clean up Product model

--- products/default/models/__init__.py
+++ products/default/models/__init__.py
@@ -1,4 +1,3 @@
 from .category_model import Category as Category
 from .products_model import ProductType as ProductType
 from .products_model import Product as Product
-from .legacy.product_image_model import ProductImage as ProductImage
--- products/default/models/products_model.py
+++ products/default/models/products_model.py
@@ -4,19 +4,7 @@ from .category_model import Category
 from .product_type_model import ProductType
 
 class Product(models.Model):
-    STATUS_CHOICES = [
-        ("ACTIVE", "Active"),
-        ("DEACTIVATED", "Deactivated"),
-    ]
-
-    METRIC_UNIT_CHOICES = [
-        ("UNIT", "Unit"),
-        ("PAIR", "Shoe pair"),
-        ("BOX", "Box")
-    ]
-
     name = models.CharField(max_length=500)
-    description = models.TextField()
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
@@ -32,29 +20,21 @@ class Product(models.Model):
         blank=True,
         related_name="products"
     )
-    
- 
+
     status = models.BooleanField(default=True)
-    brand = models.CharField(max_length=500)
-    metric_unit = models.CharField(
-        max_length=10,
-        choices=METRIC_UNIT_CHOICES,
-        default="UNIT"
-    )
     selling_price = models.DecimalField(
         max_digits=10,
         decimal_places=2
     )
     tags = TaggableManager(blank=True)
     thumbnail = models.CharField(max_length=1000)
-    class Meta:
-            db_table = "core_product"
-
     tax_rate = models.DecimalField(
             max_digits=5,
             decimal_places=4,
             default=0.18
         )
+    class Meta:
+            db_table = "core_product"
 
     def __str__(self):
         return self.name
