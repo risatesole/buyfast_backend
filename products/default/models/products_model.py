d60546c5595728370cefaf91a375b4f824466bde
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-07-08 11:24:24 -0400

    refactor(products): extract Category model into separate file

--- products/default/models/products_model.py
+++ products/default/models/products_model.py
@@ -1,19 +1,6 @@
 from django.db import models
 from taggit.managers import TaggableManager
-
-class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField(blank=True)
-    image = models.CharField(max_length=1000, blank=True)
-    status = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = "Categories"
-        db_table = "core_product_category"
+from .category_model import Category
 
 class ProductType(models.Model):
     name = models.CharField(max_length=500)
